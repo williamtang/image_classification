fc26b7758d2aa672128347e0460d81b04ba4f259
williamtang <tangweilin2087@gmail.com>
2017-10-07 17:09:04 +0800

    first commit

--- helper.py
+++ helper.py
@@ -16,7 +16,7 @@ def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
     Load a batch of the dataset
     """
     with open(cifar10_dataset_folder_path + '/data_batch_' + str(batch_id), mode='rb') as file:
-        batch = pickle.load(file, encoding='latin1')
+        batch = pickle.load(file)
 
     features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
     labels = batch['labels']
